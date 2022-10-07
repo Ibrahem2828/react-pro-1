4cca778b6273a29322f524192c2f2eec979f28d0
ibrahem <ibrahemsa28@gmail.com>
2022-10-07 10:51:54 +0300

    first react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Characters from "./components/Body/Characters"
+import './Bootstrap/dist/css/bootstrap.min.css';
+import  './Bootstrap/dist/js/bootstrap.bundle';
+import CharactersCard from "./components/Body/CharactersCard";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Characters />
+    
+    </>
+  )
 }
 
 export default App;
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+// import Caracter from './components/Body/Characters'
+// import CharactersCard from './components/Body/CharactersCard';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//        <Caracter/>
+//        <CharactersCard/>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
